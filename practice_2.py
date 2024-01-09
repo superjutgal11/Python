393892a807a02d42bebcae9d3d9342b110d77b12
JaeHyeong <153871446+superjutgal11@users.noreply.github.com>
2024-01-10 04:30:09 +0900

    python_practice_2
    문자 자료형 / boolean 자료형

---
+++ practice_2.py
@@ -0,0 +1,16 @@
+# 2강 _ 문자 자료형 / boolean 자료형
+
+# 문자형 데이터는 ' ' 또는 " " 로 표현이 가능하다.
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*8) # 이런식으로 여러 문자열을 출력이 가능하다.
+
+# boolean 형은 true 또는 False 형으로 표현되는 자료형이다.
+print(5<10)
+print(5>10)
+print(True)
+print(False)
+print(not True) # not 은 해당 bool 값의 반대값을 출력한다.
+print(not False)
+print(not(10<4)) # 프린트문 안에서 이런 논리연산도 가능하다.
