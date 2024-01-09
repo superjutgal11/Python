98440564eb0702f60385dce06e41d3e1660c7612
JaeHyeong <153871446+superjutgal11@users.noreply.github.com>
2024-01-10 04:35:44 +0900

    python_practice_6
    산술연산 축약식

---
+++ practice_6.py
@@ -0,0 +1,25 @@
+# 6강 _ 산술연산 축약식
+
+number = 2 + 3 * 4 # 14가 number 변수에 들어감
+print(number)
+
+number = number + 2 # number 값에 2 더하기
+print(number)
+
+number += 2 # number = number + 2 를 += 로 줄여쓸 수 있음
+print(number)
+
+number -= 2 
+print(number)
+
+number /= 8
+print(number)
+
+number *= 5
+print(number)
+
+number %= 4
+print(number)
+
+number **= 3
+print(number)
